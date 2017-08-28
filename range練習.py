4c62a51cc9607d51f1248b9f802a5fd4e69acc09
auxo <auxo86@gmail.com>
2017-08-28 18:43:07 +0800

    range練習

---
+++ range練習.py
@@ -0,0 +1,8 @@
+startRange = range(0,20)
+print [i for i in startRange]
+
+startRangeBy2 = range(0,20,2)
+print [i for i in startRangeBy2]
+
+startRangeBy3 = range(0,20,3)
+print [i*0.8 for i in startRangeBy3]
