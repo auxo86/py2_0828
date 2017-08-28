faaf850ee5ae43ee22d748c7616862be42314109
auxo <auxo86@gmail.com>
2017-08-28 18:46:08 +0800

    # 裝了numpy後才可以有累加0.5的index startRange4 = np.arange(0, 10, 0.5) print [i for i in startRange4]

--- range練習.py
+++ range練習.py
@@ -1,8 +1,14 @@
-startRange = range(0,20)
+import numpy as np
+
+startRange = range(0, 20)
 print [i for i in startRange]
 
-startRangeBy2 = range(0,20,2)
+startRangeBy2 = range(0, 20, 2)
 print [i for i in startRangeBy2]
 
-startRangeBy3 = range(0,20,3)
-print [i*0.8 for i in startRangeBy3]
+startRangeBy3 = range(0, 20, 3)
+print [i * 0.8 for i in startRangeBy3]
+
+# 裝了numpy後才可以有累加0.5的index
+startRange4 = np.arange(0, 10, 0.5)
+print [i for i in startRange4]
